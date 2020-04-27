362d7ab883950626dc1acdcd7921d0664bf87011
samarth <samarthmchaudhari@gmail.com>
2020-04-27 22:55:53 +0530

    Mobile App | Create UI for Health Worker

---
+++ mobile_app/app/src/main/java/com/example/coaid/SPHomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.coaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SPHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sphome_screen);
+    }
+}
---
+++ mobile_app/app/src/main/java/com/example/coaid/VHomeScreen.java
@@ -0,0 +1,13 @@
+package com.example.coaid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VHomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vhome_screen);
+    }
+}
